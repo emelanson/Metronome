391cb79a89e854d3922d5ce2e29fac6e8887378d
emelanson <44324705+emelanson@users.noreply.github.com>
2018-10-20 15:29:01 -0400

    Update Metronome.py

--- Metronome.py
+++ Metronome.py
@@ -1,27 +1,3 @@
-# import time
-# import winsound
-
-# def main(bpm = int(input('bpm: ')), bpb = int(input('bpb: ')), denom = int(input('beat value: '))  ):
-# 	#bpm = setBPM()
-
-# 	tick = bpm * (denom / 4)
-# 	sleep = 60 / tick
-# 	counter = bpb - 1
-# 	while True:
-# 		counter += 1
-# 		if counter % bpb:
-# 			winsound.PlaySound('low.wav', winsound.SND_FILENAME|winsound.SND_ASYNC)
-# 			print('tick')
-# 		else:
-# 			winsound.PlaySound('hi.wav', winsound.SND_FILENAME|winsound.SND_ASYNC)
-# 			print("TICK")
-			
-# 		time.sleep(sleep)
-
-
-# main()	
-
-
 import time
 import winsound
 #main needs beats per minute, beats per bar, and value of one beat
